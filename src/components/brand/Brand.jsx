9d3bcd34154b4899478cad208f71f7e5a1bbca02
CKMolinga <ck.molinga@gmail.com>
2022-11-17 11:24:44 +0100

    created brand section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+
+// import all images from imports.js
+import { google, slack, shopify, atlassian, dropbox } from "./imports";
 
 import "./brand.css";
 
 const Brand = () => {
   return (
-    <div>Brand</div>
-  )
-}
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="google" />
+      </div>
+      <div>
+        <img src={atlassian} alt="google" />
+      </div>
+      <div>
+        <img src={dropbox} alt="google" />
+      </div>
+      <div>
+        <img src={shopify} alt="google" />
+      </div>
+    </div>
+  );
+};
 
-export default Brand
+export default Brand;
